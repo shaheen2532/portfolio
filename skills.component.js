311759b7782816f8f78d5f7151cca793a859dfa6
MUHAMMED SHAHEEN <shaheen2532@gmail.com>
2023-09-19 18:28:14 +0530

    files

---
+++ skills.component.js
@@ -0,0 +1,11 @@
+const cssList = document.getElementsByClassName("hc");
+
+document.onload = setTimeout(blurIn, 1000); 
+
+function blurIn() {
+    console.log("JDBHDHD"); 
+    cssList.removeAttribute("id");
+}
+
+
+console.log("SCRIPT LOADED!")
